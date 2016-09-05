3653858cc25fcd147710494c08f57cc64b5e596b
Christoffer Niska <christofferniska@gmail.com>
2016-09-05 16:49:55 +0300

    Refactor contacts service

--- src/app/contacts.service.ts
+++ src/app/contacts.service.ts
@@ -3,22 +3,25 @@ import { Http } from '@angular/http';
 import { Observable } from 'rxjs';
 import { Contact } from "./models/contact";
 
-const API_ENDPOINT = '//localhost:4201/api';
-
 @Injectable()
 export class ContactsService {
+
+  private API_ENDPOINT = '//localhost:4201/api';
 
   constructor(private http: Http) {}
 
   getContacts(): Observable<Contact[]> {
-    return this.http.get(`${API_ENDPOINT}/contacts`)
-      .map((res) => res.json())
+    return this.callApi('contacts')
       .map((data) => data.items);
   }
 
   getContactById(id: number): Observable<Contact> {
-    return this.http.get(`${API_ENDPOINT}/contacts/${id}`)
-      .map((res) => res.json())
+    return this.callApi(`contacts/${id}`)
       .map((data) => data.item);
+  }
+
+  callApi(url) {
+    return this.http.get(`${this.API_ENDPOINT}/${url}`)
+      .map((res) => res.json());
   }
 }
