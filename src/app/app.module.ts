0a8a271645e46a2e10164f6e530efe08e21dd62a
Christoffer Niska <christofferniska@gmail.com>
2016-09-05 14:43:10 +0300

    Add initial support for routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { ContactsAppComponent } from './contacts.component';
 import { ContactsHeaderComponent } from './contacts-header/contacts-header.component'
 import { ContactsService } from './contacts.service';
+import { ContactsListComponent } from './contacts-list/contacts-list.component';
+import { ContactsAppRoutes } from "./app.routes";
 
 @NgModule({
   declarations: [
     ContactsAppComponent,
-    ContactsHeaderComponent
+    ContactsHeaderComponent,
+    ContactsListComponent
   ],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(ContactsAppRoutes)
+  ],
   bootstrap: [ContactsAppComponent],
   providers: [
     ContactsService // or { provide: ContactsService, useClass: ContactsService }
---
+++ src/app/app.routes.ts
@@ -0,0 +1,6 @@
+import { Routes } from "@angular/router";
+import { ContactsListComponent } from './contacts-list';
+
+export const ContactsAppRoutes: Routes = [
+  { path: '', component: ContactsListComponent }
+];
