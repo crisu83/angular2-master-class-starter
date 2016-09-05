8734c511b617aaf852376e42908912a390072688
Christoffer Niska <christofferniska@gmail.com>
2016-09-05 10:34:54 +0300

    Add contacts header component to application

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { ContactsAppComponent } from './contacts.component';
+import { ContactsHeaderComponent } from './contacts-header/contacts-header.component'
 
 @NgModule({
-  declarations: [ContactsAppComponent],
   imports: [BrowserModule],
+  declarations: [ContactsAppComponent, ContactsHeaderComponent],
   bootstrap: [ContactsAppComponent]
 })
 export class ContactsModule {
--- src/app/contacts.component.ts
+++ src/app/contacts.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'trm-contacts-app',
-  templateUrl: 'contacts.component.html',
+  template: '<trm-contacts-header></trm-contacts-header>',
   styleUrls: ['contacts.component.css']
 })
 export class ContactsAppComponent {
