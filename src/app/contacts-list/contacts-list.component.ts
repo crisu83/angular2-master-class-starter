27553df4e2229e9147f9d5bafa817c6eb5c5572e
Christoffer Niska <christofferniska@gmail.com>
2016-09-06 10:56:46 +0300

    Change to use an observable subject for the contacts list search term

--- src/app/contacts-list/contacts-list.component.ts
+++ src/app/contacts-list/contacts-list.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 
 import { ContactsService } from '../contacts.service';
 import { Contact } from '../models/contact';
-import { Observable } from 'rxjs';
+import { Observable, Subject } from 'rxjs';
 
 @Component({
   selector: 'trm-contacts-list',
@@ -13,13 +13,20 @@ export class ContactsListComponent implements OnInit {
 
   contacts: Observable<Contact[]>;
 
+  private terms$: Subject<string> = new Subject<string>();
+
   constructor(private contactsService: ContactsService) {}
 
   ngOnInit() {
     this.contacts = this.contactsService.getContacts();
+
+    this.terms$
+      .debounceTime(400)
+      .distinctUntilChanged()
+      .subscribe((term) => this.search(term));
   }
 
-  search(term: string) {
+  private search(term: string) {
     this.contacts = this.contactsService.search(term);
   }
 
